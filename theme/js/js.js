cd55b2b17f3aa465374e21a89b25ae51efb88f2d
Setikx <seda.wow@gmail.com>
2017-03-01 16:08:46 +0300

    done

--- theme/js/js.js
+++ theme/js/js.js
@@ -1,38 +1,18 @@
 (function ($) {
 
     $(window).load(function () {
-      // changeArrow();
-        onClickAroma();
-        });
-
-    function changeArrow() {
-        $(".slide-4 .flex-direction-nav a").hasClass(".with-red-arrow");
-    }
-
-
-    $(window).load(function () {
         $('.flexslider').flexslider({
             animation: "fade",
             controlNav: false,
-            slideshow: false
+            slideshow: false,
+            mousewheel: true
+
         });
-    });
-
-    function onClickAroma () {
-        var $aroma = $('li.slide-10.slide div.inner div.content div.essence ul li:last-child');
-        var $aromaSpeech = $('li.slide-10.slide div.inner div.content div.essence .small-speech');
-
-        $(window).bind('click', function(){
-            var $aromaClick = $aroma.onclick();
-
-            if($aromaClick){
-                $aromaSpeech.css('visibility', 'visible');
-            }else{
-                $aromaSpeech.css('visibility', 'hidden');
-            }
         });
 
-    }
+
+
+
 
 
 })(jQuery);
